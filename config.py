18622fe82ef99b031b09f965705461535b508e61
Jasmin Johal <jjohal52@gmail.com>
2018-08-09 20:58:59 -0700

    Removed config

---
+++ config.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+consumer_key = "Exsidq0IoMPMYaoGj0DkuFNhr"
+consumer_secret = "bMZUXBk8TP5XAPBWXhr46xvMBAbzHc7Z2WS8DKO0EHHLMIBRWd"
+access_token = "976710776483332096-ZBWW8DaFWS4VWD7bA4MZbvDUkaPZt0v"
+access_token_secret = "6rDw8c1LzNedMTPPU47Q65y0yiZ6lPMK5q9jYab7xqYUe"
+weather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
